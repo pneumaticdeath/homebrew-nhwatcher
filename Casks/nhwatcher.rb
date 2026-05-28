25192b86e182a668171e9a357a9fbdcba3f0a6f0
Mitch Patenaude <mitch@mitchpatenaude.net>
2026-05-28 14:44:29 -0700

    Testing more fixes

--- Casks/nhwatcher.rb
+++ Casks/nhwatcher.rb
@@ -1,6 +1,6 @@
 cask "nhwatcher" do
-  version "0.2.16"
-  sha256 "862c020438a62c879b602fef16680ec85c227dfb1e46fc2a245469e2f99c8bd3"
+  version "0.2.17"
+  sha256 "3e90a0b425413a0a4584ac26f9f700116cf353ca5a568460d36d045065881bb0"
 
   url "https://github.com/pneumaticdeath/NHWatcher/releases/download/v#{version}/NHWatcher-#{version}.saver.zip"
   name "NH Watcher"
